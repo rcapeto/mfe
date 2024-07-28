e87e1305f6dbe921662489fac333bca55d56e769
Raphael Capeto <raphaelcapeto@gmail.com>
2024-07-28 19:12:01 -0300

    feat: adicionando mfe-2 no shell do angular

--- shell-angular/src/app/components/main/main.component.ts
+++ shell-angular/src/app/components/main/main.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { remoteEntries } from 'src/utils/remoteEntry';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+  @ViewChild('mfe2_card', { read: ViewContainerRef })
+  mfe2Card!: ViewContainerRef
 
+  constructor() {}
+
+  async ngAfterViewInit() {
+    await Promise.all([
+      this.loadRemoteEntry()
+    ])
+  }
+
+  async loadRemoteEntry() {
+    try {
+      const module = await loadRemoteModule({
+        type: 'module',
+        remoteEntry: remoteEntries.mfe2.url,
+        exposedModule: remoteEntries.mfe2.remoteName.CardComponent
+      })
+
+      if(module && module.CardComponent) {
+        const cardComponent = this.mfe2Card.createComponent(module.CardComponent, {
+          
+        })
+
+        cardComponent.setInput('href', 'mfe2')
+      }
+
+    } catch(err) {
+      console.log(`Error get mfe2-card`, err)
+    }
+  }
 }
