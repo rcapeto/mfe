82b6fc054c8737e7905a28953bac881b04cbc401
Raphael Capeto <raphaelcapeto@gmail.com>
2024-07-28 21:24:29 -0300

    feat: implementando mini-shell-angular no shell-react

--- shell-react/src/App.tsx
+++ shell-react/src/App.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 import { MiniShellAngularCard } from './microfrontends/mini-shell-angular/Card'
+import { MiniShellReactCard } from './microfrontends/mini-shell-react'
 
 export default function App() {
   return(
@@ -7,6 +8,11 @@ export default function App() {
       <h1>Shell - React</h1>
 
       <div className="cards-container">
+        <MiniShellReactCard 
+          hrefMfe1Card="mfe1/page1"
+          hrefMfe2Card="mfe2/page1"
+        />
+
         <MiniShellAngularCard />
       </div>
     </div>
